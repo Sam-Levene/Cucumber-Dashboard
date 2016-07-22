65828066c7b2ff48faa55547a96c60b2d703ab73
AlexMurphy <alex.murphy@sogeti.com>
2016-07-22 09:57:45 +0100

    added snaphot of last run functionality

--- jobs/realdata.rb
+++ jobs/realdata.rb
@@ -1,9 +1,9 @@
 require 'mysql2'
 
-SCHEDULER.every '15m', :first_in => 0 do |job|
+SCHEDULER.every '2s', :first_in => 0 do |job|
 
 	connection =  Mysql2::Client.new(:host => "localhost", :port => 3306, :username => "cucumberuser", :password => "sogeti", :database => "cucumber")
-	results = connection.query("SELECT tr.id AS ID, s.scenario_name AS Name, tr.passed As PassedFailed, s.failure_rate AS FailureRate FROM scenario_test_runs tr, scenarios s WHERE s.id = tr.scenario_id")
+	results = connection.query("SELECT str.id AS ID, s.scenario_name AS Name, str.passed As PassedFailed, s.failure_rate AS FailureRate  FROM `scenario_test_runs` str, `time` ti, `scenarios` s WHERE  s.id = str.scenario_id AND str.test_run_at BETWEEN ti.starttime AND ti.endtime ORDER BY ti.endtime DESC LIMIT 1")
 
 	myItems = results.map do |row|
 		if row['PassedFailed'].to_s == "1" 
@@ -13,8 +13,7 @@ SCHEDULER.every '15m', :first_in => 0 do |job|
 		end
 
 		row = {
-			:label => row['ID'],
-			:value => " File: " << row['File'].to_s << "Test ID: " << row['ID'].to_s << " Scenario Name: " << row['Name'].to_s << " Test Passed/Failed: " << passorfail << " Failure Rate: " << row['FailureRate'].to_s << "%"
+			:value => "Test ID: " << row['ID'].to_s << " Scenario Name: " << row['Name'].to_s << " Test Passed/Failed: " << passorfail << " Failure Rate: " << row['FailureRate'].to_s << "%"
 		}
 	end
 
--- jobs/synergy.rb
+++ jobs/synergy.rb
@@ -1,20 +1,20 @@
 require 'mysql2'
 
-SCHEDULER.every '15m', :first_in => 0 do |job|
+SCHEDULER.every '2s', :first_in => 0 do |job|
 
 	connection =  Mysql2::Client.new(:host => "localhost", :port => 3306, :username => "cucumberuser", :password => "sogeti", :database => "cucumber")
-	results1 = connection.query("SELECT COUNT(tr.passed) As NumTestsPassed FROM scenario_test_runs tr WHERE tr.passed  = 1")
-	results2 = connection.query("SELECT COUNT(tr.passed) As NumTestsFailed FROM scenario_test_runs tr WHERE tr.passed = 0")
+	results1 = connection.query("SELECT COUNT(str.passed) As NumTestsPassed FROM scenario_test_runs str, `time` ti WHERE str.passed  = 1 AND str.test_run_at BETWEEN ti.starttime AND ti.endtime")
+	results2 = connection.query("SELECT COUNT(str.passed) As NumTestsFailed FROM scenario_test_runs str, `time` ti WHERE str.passed  = 0 AND str.test_run_at BETWEEN ti.starttime AND ti.endtime")
 	results3 = results1.first['NumTestsPassed'] + results2.first['NumTestsFailed']
 	results4 = connection.query("SELECT MAX(tr.scenario_id) As NumScenarios FROM scenario_test_runs tr")
 	testspassed = ((results1.first['NumTestsPassed'].to_f / results3.to_f) * 100.to_f).round(2)
 
 	connection.close
 
-  send_event('synergy',   { value: results1.first['NumTestsPassed'] })
-  send_event('synergy2',	{ value: results2.first['NumTestsFailed'] })
+  # send_event('synergy',   { value: results1.first['NumTestsPassed'] })
+  # send_event('synergy2',	{ value: results2.first['NumTestsFailed'] })
   send_event('synergy3',  { value: testspassed 											})
-  send_event('synergy4',	{ value: results4.first['NumScenarios']		})
+  # send_event('synergy4',	{ value: results4.first['NumScenarios']		})
 end
 
 #Synergy:
