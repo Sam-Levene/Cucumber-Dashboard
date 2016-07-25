0a1f049d1c76b4365dba16bbfafc2851c7005b51
AlexMurphy <alex.murphy@sogeti.com>
2016-07-25 13:08:47 +0100

    modifed jobs

--- jobs/realdata.rb
+++ jobs/realdata.rb
@@ -3,7 +3,7 @@ require 'mysql2'
 SCHEDULER.every '2s', :first_in => 0 do |job|
 
 	connection =  Mysql2::Client.new(:host => "localhost", :port => 3306, :username => "cucumberuser", :password => "sogeti", :database => "cucumber")
-	results = connection.query("SELECT str.id AS ID, s.scenario_name AS Name, str.passed As PassedFailed, s.failure_rate AS FailureRate  FROM `scenario_test_runs` str, `time` ti, `scenarios` s WHERE  s.id = str.scenario_id AND str.test_run_at BETWEEN ti.starttime AND ti.endtime ORDER BY ti.endtime DESC LIMIT 1")
+	results = connection.query("SELECT str.id AS ID, s.scenario_name AS Name, str.passed As PassedFailed, s.failure_rate AS FailureRate  FROM `scenario_test_runs` str, `time` ti, `scenarios` s WHERE  s.id = str.scenario_id AND str.updated_at BETWEEN ti.starttime AND ti.endtime")
 
 	myItems = results.map do |row|
 		if row['PassedFailed'].to_s == "1" 
