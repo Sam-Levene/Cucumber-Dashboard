2476c2be80d01f583b961df07194ff443ba1725b
Levene <samuel.levene@sogeti.com>
2016-08-15 10:54:31 +0100

    Initial Commit

--- jobs/failed.rb
+++ jobs/failed.rb
@@ -3,7 +3,7 @@ require 'mysql2'
 SCHEDULER.every '2s', :first_in => 0 do |job|
 
 	connection =  Mysql2::Client.new(:host => "localhost", :port => 3306, :username => "cucumberuser", :password => "sogeti", :database => "cucumber")
-	results = connection.query("SELECT sfl.id AS ID,sfl.scenario_file AS File, sfl.scenario As Scenario, sfl.failed_step AS FailedStep FROM scenario_failed_links sfl, time ti WHERE sfl.updated_at BETWEEN ti.starttime AND ti.endtime")	
+	results = connection.query("SELECT sfl.id AS ID,sfl.scenario_file AS File, sfl.scenario As Scenario, sfl.failed_step AS FailedStep FROM scenario_failed_links sfl, time ti WHERE sfl.updated_at BETWEEN ti.starttime AND ti.endtime")
 
 
 	myItems = results.map do |row|
--- jobs/realdata.rb
+++ jobs/realdata.rb
@@ -6,14 +6,14 @@ SCHEDULER.every '2s', :first_in => 0 do |job|
 	results = connection.query("SELECT str.id AS ID, s.scenario_name AS Name, str.passed As PassedFailed, s.failure_rate AS FailureRate  FROM `scenario_test_runs` str, `time` ti, `scenarios` s WHERE  s.id = str.scenario_id AND str.updated_at BETWEEN ti.starttime AND ti.endtime")
 
 	myItems = results.map do |row|
-		if row['PassedFailed'].to_s == "1" 
-			passorfail = "Pass"
+		if row['PassedFailed'].to_s == "1"
+			passorfail = "Pass."
 			else
-				passorfail ="Fail"
+				passorfail ="Fail."
 		end
 
 		row = {
-			:value => "Test ID: " << row['ID'].to_s << " Scenario Name: " << row['Name'].to_s << " Test Passed/Failed: " << passorfail << " Failure Rate: " << row['FailureRate'].to_s << "%"
+			:value => "Scenario Name: " << row['Name'].to_s << "\n Test Passed/Failed: " << passorfail << "\n Failure Rate: " << row['FailureRate'].to_s << "%"
 		}
 	end
 
